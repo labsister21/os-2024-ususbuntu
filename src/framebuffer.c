73e440a75d5a908b8cd7687286e03e8f5457f70b
SandWithCheese <naufalahmad022@gmail.com>
2024-03-31 13:06:12 +0700

    fix: fix integer overflow and indexing issue in framebuffer

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -10,24 +10,24 @@ void framebuffer_set_cursor(uint8_t r, uint8_t c) {
 
     uint16_t pos = r * 80 + c;
     out(CURSOR_PORT_CMD, 0x0F);
-    out(CURSOR_PORT_DATA, (uint8_t) (pos & 0xFF));
+    out(CURSOR_PORT_DATA, (uint8_t)(pos & 0xFF));
     out(CURSOR_PORT_CMD, 0x0E);
-    out(CURSOR_PORT_DATA, (uint8_t) ((pos>>8) & 0xFF));
+    out(CURSOR_PORT_DATA, (uint8_t)((pos >> 8) & 0xFF));
 }
 
 void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
     // TODO : Implement
-    
-    uint8_t position = row * 80 + col;
-    uint8_t color = (bg<<4) | (fg&0x0F);
-    FRAMEBUFFER_MEMORY_OFFSET[position * 2] = c ;
-    FRAMEBUFFER_MEMORY_OFFSET[position * 2 + 1] = color ;
+
+    uint16_t position = row * 80 + col;
+    uint8_t color = (bg << 4) | (fg & 0x0F);
+    FRAMEBUFFER_MEMORY_OFFSET[position * 2] = c;
+    FRAMEBUFFER_MEMORY_OFFSET[position * 2 + 1] = color;
 }
 
 void framebuffer_clear(void) {
     // TODO : Implement
-    for (int i=0; i<80; i++){
-        for (int j=0; j<25; j++){
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 80; j++) {
             framebuffer_write(i, j, 0x00, 0x07, 0x00);
         }
     }
