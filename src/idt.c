9509c99836cf224d8227d414667a3e3ab274f199
SandWithCheese <naufalahmad022@gmail.com>
2024-04-03 19:28:30 +0700

    fix: fixed idtr size and adjust keyboard map

--- src/idt.c
+++ src/idt.c
@@ -5,10 +5,10 @@ struct InterruptDescriptorTable interrupt_descriptor_table = {
     .table = {},
 };
 
-void *isr_stub_table[ISR_STUB_TABLE_LIMIT];
+void* isr_stub_table[ISR_STUB_TABLE_LIMIT];
 
 struct IDTR _idt_idtr = {
-    .size = sizeof(interrupt_descriptor_table),
+    .size = sizeof(interrupt_descriptor_table) - 1,
     .address = &interrupt_descriptor_table,
 };
 
@@ -35,11 +35,11 @@ void initialize_idt(void)
 
 void set_interrupt_gate(
     uint8_t int_vector,
-    void *handler_address,
+    void* handler_address,
     uint16_t gdt_seg_selector,
     uint8_t privilege)
 {
-    struct IDTGate *idt_int_gate = &interrupt_descriptor_table.table[int_vector];
+    struct IDTGate* idt_int_gate = &interrupt_descriptor_table.table[int_vector];
 
     // TODO : Set handler offset, privilege & segment
     // Use &-bitmask, bitshift, and casting for offset
@@ -51,7 +51,7 @@ void set_interrupt_gate(
 
     idt_int_gate->privilege = privilege;
     idt_int_gate->segment = gdt_seg_selector;
-    
+
     // Target system 32-bit and flag this as valid interrupt gate
     idt_int_gate->_r_bit_1 = INTERRUPT_GATE_R_BIT_1;
     idt_int_gate->_r_bit_2 = INTERRUPT_GATE_R_BIT_2;
